15b3c579116b95aa51c01acffc323f7b9a793ff9
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2021-04-06 16:31:01 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 /**
  * Interprete
+ *@author Angel HIgueros
+ *@author Mynor Lima
+ *@author Fredy Velasquez
+ *@version 1.0
  */
 
-public class Main {
+public class Main { //Clase que ejecuta todo
 
     public static void main(String[] args) {
 
         // Utilidades
-        Interprete interprete = new Interprete();
-
+        Interprete interprete = new Interprete(); //Instancio Interprete
+        
+        //Bienvenida
         System.out.print("\n\n :: INTERPRETE LISP :: \n");
-        interprete.ejecutar();
+        interprete.ejecutar(); //Llamo a lo que tengo dentro de interprete con el metodo ejecutar
 
     }
 }
