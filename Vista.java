3e6eebe6d83b087090252664fc9a3f5689688f0c
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2021-04-06 16:41:49 -0600

    Update Vista.java

--- Vista.java
+++ Vista.java
@@ -1,14 +1,23 @@
-public class Vista {
+/*
+*@author Angel Higueros
+*@author Fredy Velasquez
+*@Mynor Lima
+*@version 1.0
+*/
 
+//Clase que me va a servir para mostrar todo lo que necesita ver el usuario
+public class Vista {
+    
+    //Aqui pongo los colores que uso para que no se mire tan TRISTE la consola
     String red = "\033[31m";
-    String green = "\033[32m";
+    String green = "\033[32m"; 
     String reset = "\u001B[0m";
 
-    public void prinrErr(String mensaje) {
+    public void prinrErr(String mensaje) { //Imprimo el mensaje que necesito con formato ROJO
         System.out.print(red + ">> " + mensaje + reset + "\n");
     }
 
-    public void print(String mensaje) {
+    public void print(String mensaje) { //Imprimo el mensaje que necesito con formato VERDE
         System.out.print(green + ">> " + mensaje + reset + "\n");
     }
 
