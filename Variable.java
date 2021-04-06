e3f8e366e89964a8d8778a21dbe7a0832f3e22e7
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2021-04-06 16:37:49 -0600

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,26 +1,40 @@
 
-public class Variable {
-    private String valor;
-    private String nombre;
+/*
+*@author Angel Higueros
+*@author Fredy Velasquez
+*@author Mynor Lima
+*@version 1.0
+*/
 
+
+
+public class Variable { //Clase que cuenta con las funcinoes de VARIABLE
+    private String valor; //Variable valor
+    private String nombre; //Variable nombre
+
+    
+    //Creo mi constructor y asigno valores a mis variables
     public Variable(String nombre, String valor) {
-        this.nombre = nombre;
-        this.valor = valor;
+        this.nombre = nombre; //Asigno
+        this.valor = valor; //Asigno
+    } 
+
+    //Creo los setters y getters respectivos para cada uno de las variables que defini arriba
+    
+    public String getNombre() { //get de Nombre
+        return nombre; //Retorno
+    } 
+                //Paso el parametro valor
+    public void setValor(String valor) { //set de Valor
+        this.valor = valor; //Asigno aqui
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getValor() { //get de Valor
+        return valor; //Retorno
     }
 
-    public void setValor(String valor) {
-        this.valor = valor;
-    }
-
-    public String getValor() {
-        return valor;
-    }
-
+    //Aca creo mi toString
     public String toString() {
-        return (valor);
+        return (valor); //Retorno mi valor
     }
 }
